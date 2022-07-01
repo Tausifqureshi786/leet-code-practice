1459caa29a967d85d23d6981be72b2721fbbd4f3
Tausif Qureshi <qamin9333@gmail.com>
2022-07-01 15:52:46 -0600

    Added two test cases for the containsDuplicate function

--- Arrays/containsDuplicate.test.js
+++ Arrays/containsDuplicate.test.js
@@ -3,3 +3,11 @@ const containsDuplicate = require("./containsDuplicate");
 test("Returns false as the provided array does not have any duplicates", () =>{
     expect(containsDuplicate([1,2,3,4])).toBe(false)
 })
+
+test("Returns true as the provided array has duplicates", () =>{
+    expect(containsDuplicate([1,2,3,1])).toBe(true)
+});
+
+test("Returns true as the provided array has duplicates", ()=> {
+    expect(containsDuplicate([1,1,1,3,3,4,3,2,4,2])).toBe(true)
+})
